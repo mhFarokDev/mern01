cffe825e14a8b748bed1de299c16fffb506bcf0b
mhFarokDev <87332293+mhFarokDev@users.noreply.github.com>
2021-11-09 19:50:09 +0600

    Add files via upload

---
+++ console.js
@@ -0,0 +1,11 @@
+console.log('My Name Is MH Faruk');
+
+console.dir("I am a Fullstack Developer");
+
+// This is template literale
+let fName='MH ';
+let lName='Faruk'
+console.log(`My name is ${fName+lName}`);
+
+console.log(`This is Faruk.
+I am a Fullstack Developer`);
